e35f9029de1c186af071a5e6fa13dcee9642efa3
Luis Remis <luis.remis@intel.com>
2018-04-16 12:22:46 -0700

    Fix run scripts

--- examples/tcia/clean_run_server.sh
+++ examples/tcia/clean_run_server.sh
@@ -2,7 +2,6 @@
 export PATH=$PATH:/vdms
 export LD_LIBRARY_PATH=/pmgd/lib:$(find /usr/local/lib/ / -type f -name "*.so" | xargs dirname | sort | uniq | tr "\n" ":")
 echo "Building database... "
-tar -xzSf /data.tgz
+tar -xzSf data.tgz
 echo "Database built!"
-mv hls-config-1.json config-vdms.json
-../../vdms 2> log.log
+../../vdms -cfg hls-config-1.json 2> log.log
